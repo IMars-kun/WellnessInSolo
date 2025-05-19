f7bbfd8f8a38ce9dd2cb62819b2e2872f4810b4a
IMars-kun <berlymarcellino25@gmail.com>
2025-05-19 18:32:00 +0700

    Update kuliner.js

--- js/kuliner.js
+++ js/kuliner.js
@@ -8,3 +8,5 @@ back.onclick = () => {
 function belumJadi(){
     alert("Website masih dalam perbaikan");
 };
+
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="jvW5wGJMzohsgCm1UMhfw";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
